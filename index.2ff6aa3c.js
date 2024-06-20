95f5cf7497e6d1da179eae08437b8aa7777d09bd
Yurii <urabelivr@gmail.com>
2024-06-20 23:57:25 +0200

    make build

---
+++ index.2ff6aa3c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__large-img");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault(),"IMG"===t.target.tagName?e.src=t.target.parentElement.href:e.src=t.target.href});
+//# sourceMappingURL=index.2ff6aa3c.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const largeImg = document.querySelector(".gallery__large-img");
+const imageList = document.querySelector(".gallery__list");
+function handleImageThumbnailClick(clickEvent) {
+    clickEvent.preventDefault();
+    if (clickEvent.target.tagName === "IMG") largeImg.src = clickEvent.target.parentElement.href;
+    else largeImg.src = clickEvent.target.href;
+}
+imageList.addEventListener("click", handleImageThumbnailClick);
+
+//# sourceMappingURL=index.f75de5e1.js.map
